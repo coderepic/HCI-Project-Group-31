b20828057a189a0356e419817ad937fcca185d95
Archit Kumar <57429607+coderepic@users.noreply.github.com>
2023-04-22 14:52:53 -0400

    Implement Main Game Loop

--- HCI/gamemanager.js
+++ HCI/gamemanager.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 function initialize(){
     document.getElementsByClassName("countryButtons").style.display = 'none';
     document.getElementById("score").style.display = 'none';
@@ -5,15 +6,107 @@ function initialize(){
 
 function startGame(){
     document.getElementById("score").style.display = 'block';
-    document.getElementById("selectCountry").style.display = 'block';
-    document.getElementById("playbutton").style.display = 'none';
-    document.getElementById("canadaButton").style.display = 'block';
-    document.getElementById("brazilButton").style.display = 'block';
-    document.getElementById("southAfricaButton").style.display = 'block';
+=======
+let score = 0;
+let questionsRemaining = 5;
+let questions = ["cnTowerQuestion", "madagascarQuestion", "tanzaniaQuestion", "indonesiaQuestion", "easterIslandQuestion", "germanyQuestion"];
+currentQuestionIndex = 0;
+
+//Code below obtained from: 
+//https://www.educative.io/answers/how-to-shuffle-an-array-in-javascript
+function shuffleQuestions(array){
+    array.sort(() => Math.random() - 0.5);
 }
 
-function startCanada(){
+function init(){
+    shuffleQuestions(questions);
+    currentQuestionIndex = 0;
+    questionsRemaining = 5;
+    score = 0;
+    //document.getElementsByClassName("countryButtons").style.display = 'none';
+    document.getElementById("scoreGroup").style.display = 'none';
+    document.getElementById("scoreVal").innerHTML = "0";
+    document.getElementById("finalScoreDisplay").style.display = 'none';
+    document.getElementById("questionsRemaining").style.display = 'none';
+    document.getElementById("playbutton").style.display = 'block';
+}
+
+function displayInstructions(){
+    document.getElementById("instructions").style.display = 'block';
+    document.getElementById("playbutton").style.display = 'none';
+}
+
+function startGame(){
+    //init();
+
+    shuffleQuestions(questions);
+    currentQuestionIndex = 0;
+    document.getElementById("instructions").style.display = 'none';
+    questionsRemaining = 5;
+    score = 0;
+    document.getElementById("scoreGroup").style.display = 'none';
+    document.getElementById("scoreVal").innerHTML = "0";
+    document.getElementById("questionsRemaining").style.display = 'none';
+>>>>>>> Stashed changes
+    document.getElementById("selectCountry").style.display = 'block';
+    document.getElementById("finalScoreDisplay").style.display = 'none';
+    document.getElementById("canadaButton").style.display = 'block';
+    document.getElementById("brazilButton").style.display = 'block';
+}
+
+function startLandmarks(){
     document.getElementById("brazilButton").style.display = 'none';
     document.getElementById("selectCountry").textContent = "Your first destination is CN Tower";
     document.getElementById("canadaButton").style.display = 'none';
+<<<<<<< Updated upstream
 }
+=======
+    document.getElementById("cnTowerQuestion").style.display = 'block';
+    document.getElementById("selectCountry").style.display = 'none';
+    document.getElementById("scoreGroup").style.display = 'block';
+    document.getElementById("scoreVal").innerHTML = score.toString();
+    document.getElementById("questionsRemaining").style.display = 'block';
+    document.getElementById("questionsRemainingElement").innerHTML = questionsRemaining.toString();
+}
+
+function resultScreen(correct){
+    //document.getElementById("cnTowerQuestion").style.display = "none";
+    questionsArray = document.getElementsByClassName("triviaQuestion");
+    for(let i=0; i<questionsArray.length; i++){
+        questionsArray[i].style.display = 'none';
+    }
+
+    if (correct == 1) {
+        score = score + 1;
+        document.getElementById("correctDisplay").style.display = "block";
+    }
+    else {
+        document.getElementById("incorrectDisplay").style.display = "block";
+    }   
+    questionsRemaining = questionsRemaining - 1;
+    document.getElementById("scoreVal").innerHTML = score.toString();
+    document.getElementById("questionsRemainingElement").innerHTML = questionsRemaining.toString();
+
+}
+
+function displayResults(){
+    document.getElementById("finalScoreDisplay").style.display = "block";
+}
+
+function loadQuestion(){
+    if(questionsRemaining <= 0){
+        displayResults();
+    }
+    else{
+        currentQuestionIndex++;
+        document.getElementById(questions[currentQuestionIndex]).style.display = "block";
+    }
+    document.getElementById("correctDisplay").style.display = "none";
+    document.getElementById("incorrectDisplay").style.display = "none";
+}
+
+// function update_test_value(){
+//     let position = positionCheck();
+//     document.getElementById(inputtest).innerHTML = position.toString();
+// }
+>>>>>>> Stashed changes
--- HCI/main.js
+++ HCI/main.js
@@ -14,6 +14,8 @@ var frames = {
         frames.socket = new WebSocket(url);
         frames.socket.onmessage = function (event) {
             frames.show(JSON.parse(event.data));
+            //My own line below:
+            updateNumber();
         }
     },
 
@@ -27,7 +29,7 @@ var twod = {
 
     // create a connection to the camera feed
     start: function () {
-        var url = "ws://" + host + "/twod";
+        var url = "ws://" + host + "/frames";
         twod.socket = new WebSocket(url);
 
         // whenever a new frame is received...
@@ -43,3 +45,32 @@ var twod = {
         $('img.twod').attr("src", 'data:image/pnjpegg;base64,' + twod.src);
     },
 };
+
+//EDITED CODE
+var positionCheck = function(){
+    if(frames.people[0] != null){
+        if(frames.people[0].joint[0].position.x < 0){
+            return "left";
+        }
+        else{
+            return "right";
+        }
+    }
+}
+
+function sendPositionToDisplay(){
+    let position = positionCheck();
+    document.getElementById("inputtest").innerHTML = position.toString();
+}
+
+let number = 0
+function updateNumber(){
+    // number++
+    // document.getElementById("inputtest").innerHTML = number;
+    if(frame.people.length > 0){
+        document.getElementById("inputtest").innerHTML = "hello!";
+    }
+    else{
+        document.getElementById("inputtest").innerHTML = "hello!";
+    }
+}
